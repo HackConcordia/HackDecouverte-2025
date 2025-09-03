b43c598c36007936d1a4f18c67a8b85dda7eb513
MasoumehF <masoumehfarokhpour.career@gmail.com>
2025-09-03 19:48:28 -0400

    - style: improve for mobile

--- src/components/DateDisplay.tsx
+++ src/components/DateDisplay.tsx
@@ -1,8 +1,10 @@
+import { useTranslation } from '@/i18n/TranslationContext';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { FaRegCalendarPlus } from 'react-icons/fa';
 
 const DateDisplay = ({ startDate, endDate } : any) => {
+  const { t } = useTranslation();
   const [formattedDate, setFormattedDate] = useState('');
   const [startISODate, setStartISODate] = useState('');
   const [endISODate, setEndISODate] = useState('');
@@ -61,13 +63,15 @@ const DateDisplay = ({ startDate, endDate } : any) => {
   }
 
   return (
-    <div className='flex justify-center flex-col items-center mt-6 md:mt-4 md:mb-3 relative'>
-      <h2 className='text-sm md:text-lg font-bold text-gray-900 dark:text-gray-100' dangerouslySetInnerHTML={{ __html: formattedDate }}></h2>
+    <div className='flex justify-center flex-col items-center mt-4 md:mt-4 md:mb-3 relative'>
+      <p className="relative text-xs md:text-sm lg:text-base mb-0 font-bold lg:px-0 mt-1 lg:mt-0">
+                {t("hero.event")}
+            </p>
       <Link
         href={`https://www.google.com/calendar/render?action=TEMPLATE&text=HackDecouverte+2025&dates=${startISODate}/${endISODate}&details=Join+us+for+HackDecouverte!+Watch+our+social+media+platforms+and+emails+for+more+details.`}
         target='_blank'
         rel='noopener noreferrer'
-        className={`hover:scale-105 active:scale-95 duration-300 group animate-fade-in-2 flex gap-1 items-center justify-center w-fit transition-all bg-blue-700 hover:bg-blue-800 dark:bg-pink-600 dark:hover:bg-pink-700 text-white py-2 px-3 rounded-sm text-xs mt-4 tracking-widest`}
+        className={`hover:scale-105 active:scale-95 duration-300 group animate-fade-in-2 flex gap-1 items-center justify-center w-fit transition-all border-2 border-[#491000] text-[#491000] font-bold hover:bg-[#320b00] py-2 px-3 rounded-sm text-xs mt-4`}
       >
         Add to calendar
         <FaRegCalendarPlus className='text-sm opacity-70 transition-all group-hover:translate-x-1' />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,10 +12,10 @@ export default function Hero() {
         title: 'HackDecouverte',
         eventStartDate: '2025-11-15T08:00:00',
         eventEndDate: '2025-11-15T19:00:00',
-      };
+    };
 
     return (
-        <section className="relative mt-10 sm:mt-0 min-h-screen flex flex-col items-center md:justify-center text-center max-w-7xl mx-auto px-4">
+        <section className="relative mt-10 sm:mt-0 lg:min-h-screen flex flex-col items-center md:justify-center text-center max-w-7xl mx-auto px-4">
 
             {/* Title */}
             <h1 className="relative text-4xl sm:text-6xl md:text-8xl font-extrabold pt-4 lg:pt-6 pb-4
@@ -29,23 +29,20 @@ export default function Hero() {
             <p className="relative text-sm md:text-xl max-w-4xl mb-2 font-bold">
                 {t("hero.subheading")}
             </p>
-            <p className="relative text-xs md:text-sm lg:text-base mb-4 font-bold px-20 lg:px-0 mt-1 lg:mt-0">
-                {t("hero.event")}
-            </p>
 
             <a
                 href="#register"
-                className="block py-3 px-5 text-center bg-[#491000] text-white font-bold rounded-sm shadow hover:bg-[#320b00] transition"
+                className="block py-3 px-5 text-center bg-[#491000] text-white font-bold rounded-sm shadow hover:bg-[#320b00] transition text-xs lg:text-base mt-2"
             >
                 {t("hero.register")}
             </a>
 
             {/* Countdown */}
             <Countdown targetDate={config.eventStartDate} />
-          <DateDisplay
-            startDate={config.eventStartDate}
-            endDate={config.eventEndDate}
-          />
+            <DateDisplay
+                startDate={config.eventStartDate}
+                endDate={config.eventEndDate}
+            />
 
             {/* HackConcordia credit */}
             <p className="mt-6 lg:mt-4 text-sm sm:text-base text-gray-800">
@@ -61,7 +58,7 @@ export default function Hero() {
             </p>
 
             {/* Spacer to push bottom */}
-            <div className="mb-100 sm:mb-100 md:mb-100 lg:mb-70" />
+            <div className="mb-20" />
             <Image
                 src="/images/globe.png"
                 alt="HackConcordia Logo"
@@ -72,9 +69,9 @@ export default function Hero() {
             <Image
                 src="/images/globe.png"
                 alt="HackConcordia Logo"
-                width={280}
-                height={200}
-                className="absolute lg:hidden block bottom-20 right-0 mb-4"
+                width={120}
+                height={100}
+                className="absolute lg:hidden block bottom-0 right-0"
             />
             <Image
                 src="/images/star.png"
@@ -86,9 +83,9 @@ export default function Hero() {
             <Image
                 src="/images/star.png"
                 alt="HackConcordia Logo"
-                width={100}
-                height={80}
-                className="absolute block lg:hidden bottom-55 left-5 lg:bottom-70 md:left-10 lg:left-45 mb-4"
+                width={70}
+                height={60}
+                className="absolute block lg:hidden bottom-5 left-5 lg:bottom-70 md:left-10 lg:left-45 mb-4"
             />
             <Image
                 src="/images/stars.png"
@@ -102,7 +99,7 @@ export default function Hero() {
                 alt="HackConcordia Logo"
                 width={60}
                 height={60}
-                className="absolute hidden md:block bottom-70 lg:right-120 mb-4 ml-4"
+                className="absolute hidden md:block bottom-80 lg:right-90 mb-4"
             />
         </section>
     );
